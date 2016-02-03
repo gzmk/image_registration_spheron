72af5b0b38addb79747b9696a3035f076dafe94b
gzmk <gizemkucukoglu@gmail.com>
2016-02-03 13:45:13 +0000

    added bounds

--- imregfit.m
+++ imregfit.m
@@ -23,6 +23,11 @@ disp('we are in')
 % build affine transformation matrix
 % for types of affine transformation matrices look here:
 % http://www.mathworks.com/help/images/performing-general-2-d-spatial-transformations.html#f12-33299
+if (p(1)>13 | p(2)<-13 | p(3) > 1.2 | p(4)> 1.2)
+    error = 10e9;
+    return;
+end
+
 affineM =[p(3) 0 0;
           0 p(4) 0;
          p(1) p(2) 1 ];
